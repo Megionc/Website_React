f67937bee9ae6cc3427fc33514d6de2194dbec5c
megionc <lyubchenko.kseniya@gmail.com>
2024-08-19 18:19:34 +0500

    MyButton

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ClassCounter from './components/ClassCounter';
 import './styles/App.css';
 import PostItem from './components/PostItem';
 import PostList from './components/PostList';
+import MyButton from './components/UI/button/MyButton';
 
 function App() {
   const [posts, setPosts] = useState([
@@ -14,6 +15,11 @@ function App() {
 
   return (
     <div className="App">
+      <form>
+        <input type="text" placeholder="Название поста" />
+        <input type="text" placeholder="Описание поста" />
+        <MyButton disabled>Создать пост</MyButton>
+      </form>
       <PostList posts={posts} title="Посты про JS" />
     </div>
   );

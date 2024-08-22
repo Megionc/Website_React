5fa19e9612771ef53fa4ea96ec9e8b8001f9d206
megionc <lyubchenko.kseniya@gmail.com>
2024-08-22 15:05:45 +0500

    MyModal

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import MyInput from './components/UI/input/MyInput';
 import PostForm from './components/PostForm';
 import MySelect from './components/UI/select/MySelect';
 import PostFilter from './components/PostFilter';
+import MyModal from './components/UI/MyModal/MyModal';
 
 function App() {
   const [posts, setPosts] = useState([
@@ -18,6 +19,7 @@ function App() {
   ])
 
   const [filter, setFilter] = useState({ sort: '', query: '' })
+  const [modal, setModal] = useState(false)
 
   const sortedPosts = useMemo(() => {
     if (filter.sort) {
@@ -32,6 +34,7 @@ function App() {
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
+    setModal(false)
   }
 
   const removePost = (post) => {
@@ -41,20 +44,19 @@ function App() {
 
   return (
     <div className="App">
-      <PostForm create={createPost} />
+      <MyButton style={{ marginTop: 30 }} onClick={() => setModal(true)}>
+        Создать пользователя
+      </MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost} />
+      </MyModal>
+
       <hr style={{ margin: '15px 0' }} />
       <PostFilter
         filter={filter}
         setFilter={setFilter}
       />
-      {sortedAndSearchedPosts.length
-        ?
-        <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS" />
-        :
-        <h1 style={{ textAlign: 'center' }}>
-          Посты не найдены!
-        </h1>
-      }
+      <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS" />
     </div>
   );
 }
